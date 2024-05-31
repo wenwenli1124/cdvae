70370b2826e4ee22f8f2ebd14d4cfed82ef4ea6f
ixsluo <xiaoshan.luo@outlook.com>
2024-05-31 09:53:59 +0800

    build: fix requirements
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- setup.py
+++ setup.py
@@ -3,5 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name='cdvae',
     version='0.01',
-    packages=find_packages(include=['cdvae'])
+    packages=find_packages(include=['cdvae']),
+    install_requires=[
+        "ase",
+        "click",
+        "hydra-core",
+        "lightning",
+        "pandas",
+        "pyarrow",
+        "pymatgen",
+        "python-dotenv",
+        "torch_geometric",
+        "torch_scatter",
+        "torch_sparse",
+        "wandb",
+    ],
 )
